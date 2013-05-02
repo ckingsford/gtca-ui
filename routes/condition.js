81d031766fd619b828a96ae9fd28254d0d7c50d0
Tom Abraham <tabraham@andrew.cmu.edu>
2013-05-02 09:16:50 -0400

    Added condition autocomplete endpoint

---
+++ routes/condition.js
@@ -0,0 +1,18 @@
+
+/*
+ * GET drugs listing.
+ */
+var drugs = [ { id: 1, name: "Atrial Fibrillation" }, { id: 2, name: "Fibromyalgia" } ];
+
+exports.list = function(req, res){
+  var q = req.query.q ? req.query.q.toLowerCase() : ""
+
+  var results = []
+  drugs.forEach(function(drug, i, array) {
+    var name = drug.name.toLowerCase();
+    if (name.slice(0, q.length) == q) {
+      results.push(drug);
+    }
+  });
+  res.send(results);
+};
